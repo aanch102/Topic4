cae8fcc3e4d62e80452f734645139854862b033e
aanchal shrestha <aanchalshrestha77@gmail.com>
2019-12-02 14:36:46 +0545

    Initial commit

---
+++ app/src/main/java/com/aanchal/topic4/CapitalActvity.java
@@ -0,0 +1,14 @@
+package com.aanchal.topic4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CapitalActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capital_actvity);
+    }
+}
